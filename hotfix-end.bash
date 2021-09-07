f1d4089826f8235f050d28e60203327530de7a48
manuel-192 <manuel-192@outlook.com>
2021-09-07 22:56:42 +0300

    added ISO version check

--- hotfix-end.bash
+++ hotfix-end.bash
@@ -2,3 +2,34 @@
 #
 # Hotfixes for the ISOs that can be used after releaseing the ISO.
 # This file is meant for fixes that need to be done at the end of running calamares.
+
+DIE() {
+    local type="$1"
+    local msg="$2"
+    echo "$progname: $type: $msg"
+    exit 1
+}
+
+Main() {
+    local progname=$(basename "$0")
+    local file=/usr/lib/endeavouros-release
+    local VERSION=""
+
+    [ -r $file ] && source $file
+
+    case "$VERSION" in
+        "2021.08.27")
+            echo "$progname: adding hotfixes after ISO $VERSION."
+            # Add hotfixes here.
+            ;;
+
+        "") DIE warning "sorry, ISO version not found."
+            ;;
+
+        *)  DIE info "no hotfixes for ISO version $VERSION."
+            ;;
+    esac
+}
+
+Main "$@"
+
