aa458d18f1ac859d9cda87d90765cfab7c202eba
manuel-192 <manuel-192@outlook.com>
2021-11-03 14:26:02 +0200

    added testing hotfix for Octobet/November 2021 ISOs

--- hotfix-end.bash
+++ hotfix-end.bash
@@ -11,21 +11,35 @@ MSG() {
 
 Main() {
     local progname=$(basename "$0")
+    source /usr/share/endeavouros/scripts/eos-script-lib-yad || return 1
     local file=/usr/lib/endeavouros-release
     local VERSION=""
 
     [ -r $file ] && source $file
 
     case "$VERSION" in
-        "2021.08.27")
+        2021.08.27)
             MSG info "hotfixes after ISO $VERSION."
             # Add hotfixes here.
+            ;;
+        2021.10.31 | 2021.11.*)
+            case "$(eos_GetDeOrWm)" in
+                SWAY)
+                    if ! pacman -Q ly >&/dev/null ; then
+                        MSG info "sway: installing ly"
+                        pacman -Syu --needed --noconfirm ly
+                    fi
+                    MSG info "sway: enabling ly service"
+                    systemctl --force enable ly
+                    ;;
+            esac
             ;;
 
         "") MSG warning "sorry, ISO version not found."
             ;;
 
-        *)  MSG info "no hotfixes found for ISO version $VERSION."
+        *)
+            MSG info "no hotfixes found for ISO version $VERSION."
             ;;
     esac
 }
