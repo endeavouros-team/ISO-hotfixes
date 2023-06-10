8643690471f12ba153c9f0017c97c506d04ab6c6
Johannes Kamprad <archlinux@kamprad.net>
2023-06-10 19:48:04 +0200

    [hotfix-start.bash]  2023.06.09 Cassini nova R2 (rebuild)

--- hotfix-start.bash
+++ hotfix-start.bash
@@ -97,6 +97,13 @@ Main() {
             SkipPackageInstallInFile packagechooser_ce.conf xcursor-neutral
             sed -i 's/ttf-nerd-fonts-symbols-2048-em/ttf-nerd-fonts-symbols/g' /etc/calamares/modules/packagechooser_ce.conf
             ;;
+        2023.06.09)  # Cassini nova R2 (rebuild)
+            # [hardwaredetect] Do not return error if hardware detection fails
+            wget -qN -P "/usr/lib/calamares/modules/hardwaredetect/" "https://raw.githubusercontent.com/endeavouros-team/calamares/01aeb60d05c864bacc926f718686c27c69b84f49/src/modules/hardwaredetect/main.py"
+            SkipPackageInstallInFile packagechooser_ce.conf xcursor-neutral
+            sed -i 's/ttf-nerd-fonts-symbols-2048-em/ttf-nerd-fonts-symbols/g' /etc/calamares/modules/packagechooser_ce.conf
+            ;;
+
         *)
             HotMsg "no hotfixes for ISO version $ISO_VERSION."
             ;;
