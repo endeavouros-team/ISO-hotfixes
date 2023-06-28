7818f08ce3c26bad2460d4fb518a4e8f5de904b2
Johannes Kamprad <archlinux@kamprad.net>
2023-06-28 12:52:31 +0200

    # [netinstall.yaml] fix cinnamon to not get xdg-desktop-portal-gnome installed  05/28

--- hotfix-start.bash
+++ hotfix-start.bash
@@ -99,6 +99,8 @@ Main() {
                                 "https://raw.githubusercontent.com/endeavouros-team/calamares/01aeb60d05c864bacc926f718686c27c69b84f49/src/modules/hardwaredetect/main.py"
             SkipPackageInstallInFile packagechooser_ce.conf xcursor-neutral
             sed -i 's/ttf-nerd-fonts-symbols-2048-em/ttf-nerd-fonts-symbols/g' /etc/calamares/modules/packagechooser_ce.conf
+            # [netinstall.yaml] fix cinnamon to not get xdg-desktop-portal-gnome installed
+            sed -i '/^    - x-apps.*/ a\    - xdg-desktop-portal-gtk' /etc/calamares/modules/netinstall.yaml
             ;;
         2023.06.09)  # Cassini nova R2 (rebuild)
             # [hardwaredetect] Do not return error if hardware detection fails
